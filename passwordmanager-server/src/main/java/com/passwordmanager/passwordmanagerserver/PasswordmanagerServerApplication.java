667a7f63c44455d7686bed58e9c922aad7d1f9c5
Ivan Nikolaev-Axenov <nikolaevaxenov@gmail.com>
2023-02-15 20:26:18 +0300

    Initial commit

---
+++ passwordmanager-server/src/main/java/com/passwordmanager/passwordmanagerserver/PasswordmanagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.passwordmanager.passwordmanagerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordmanagerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordmanagerServerApplication.class, args);
+    }
+
+}
---
+++ passwordmanager-server/src/test/java/com/passwordmanager/passwordmanagerserver/PasswordmanagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passwordmanager.passwordmanagerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordmanagerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
